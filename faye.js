e391fb43f8ea84ccb8a5ab5e4fcdf4008b902cf5
jhjguxin <jhjguxin@gmail.com>
2020-08-11 20:50:43 +0800

    升级 faye 到最新版本
    开启详细日志到标准输出

--- faye.js
+++ faye.js
@@ -7,28 +7,35 @@ const http = require('http'),
   yaml = require('js-yaml'),
   redisFilePath = "config/redis.yml";
 
-let engineConf;
+let engineConf = {};
 
 if ( fs.existsSync(redisFilePath) ) {
   engineConf = yaml.safeLoad(fs.readFileSync(redisFilePath)) || {};
-  if ( engineConf.host ) {
-    engineConf.type = fayeRedis;
-  }
+  engineConf.type = fayeRedis;
 }
+
 
 if ( !fs.existsSync("tmp") ) fs.mkdirSync("tmp");
 if ( !fs.existsSync("tmp/pids") ) fs.mkdirSync("tmp/pids");
 
 fs.writeFileSync(`tmp/pids/faye.${port}.pid`, String(process.pid));
 
+// REVIEW 开启详细日志到标准输出
+// faye.logger = console;
+
 let server = http.createServer();
 let bayeux = new faye.NodeAdapter({
   mount:    '/faye',
-  timeout:  45,
+  timeout:  3,
   engine:   engineConf
 });
 
-console.log(`started on localhost:${port}, PID: ${process.pid}`)
+console.log(`started on localhost:${port}, PID: ${process.pid}`);
+
+server.on('error', function (e) {
+  // Handle your error here
+  console.error(err.stack);
+});
 
 bayeux.attach(server);
 server.listen(port);
